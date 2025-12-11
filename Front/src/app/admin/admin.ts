2f36517b4213e34ebe55e883afa894194f3a6a47
ErickATE1306 <arnoldantezanatorres@gmail.com>
2025-12-11 12:16:22 -0500

    listo

--- Front/src/app/admin/admin.ts
+++ Front/src/app/admin/admin.ts
@@ -1,16 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../compartido/servicios/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './admin.html',
   styleUrl: './admin.scss',
 })
 export class Admin {
   private authService = inject(AuthService);
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
 
   logout() {
     this.authService.logout();
--- Front/src/app/empleado/empleado.ts
+++ Front/src/app/empleado/empleado.ts
@@ -1,16 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../compartido/servicios/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-empleado',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './empleado.html',
   styleUrl: './empleado.scss',
 })
 export class Empleado {
   private authService = inject(AuthService);
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
 
   logout() {
     this.authService.logout();
